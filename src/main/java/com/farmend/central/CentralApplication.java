4dd316b09b847bb962e2687bec17669b91b784e3
dong3114 <scv3114@gmail.com>
2024-08-09 01:49:25 +0900

    feat: main first comit

---
+++ src/main/java/com/farmend/central/CentralApplication.java
@@ -0,0 +1,14 @@
+package com.farmend.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralApplication.class, args);
+    }
+}
